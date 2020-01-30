33800f205961757edda18152ca9d2e8dcacf6095
Joseph Summerhays <joseph.summerhays@byu.edu>
2020-01-29 17:21:06 -0700

    Changes during meeting

--- baseCNN.py
+++ baseCNN.py
@@ -1,3 +1,6 @@
+import torchvision.models as models
+from torch import nn
+
 class ConvNetwork(nn.Module):
     def __init__(self, data, numClasses):
         super(ConvNetwork, self).__init__()
@@ -5,15 +8,16 @@ class ConvNetwork(nn.Module):
         channels, height, width = x.size()
         output = numClasses
 
-        self.net = nn.Sequential(
-            Conv2d(channels, 10, (3,3), padding=(1,1)),
-            nn.ReLU(),
-            Conv2d(10, 100, (5,5), padding=(1,1)),
-            nn.ReLU(),
-            Conv2d(100, 300, (5,5), padding=(1,1)),
-            nn.ReLU(),
-            Conv2d(300, output, (24,24), padding=(0,0))
-        )
+        self.net = models.vgg19(pretrained=True)
+#        self.net = nn.Sequential(
+#            nn.Conv2d(channels, 10, (3,3), padding=(1,1)),
+#            nn.ReLU(),
+#            nn.Conv2d(10, 100, (5,5), padding=(1,1)),
+#            nn.ReLU(),
+#            nn.Conv2d(100, 300, (5,5), padding=(1,1)),
+#            nn.ReLU(),
+#            nn.Linear()
+#        )
 
     def forward(self,x):
         return self.net(x)
