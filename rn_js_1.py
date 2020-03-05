6432e922328fa5c3758c395cef4c0d2faef298ac
Skyler <skylerg022@gmail.com>
2020-03-04 20:41:24 -0700

    Variable Setting, Data Parallelism, Saving Added

--- rn_js_1.py
+++ rn_js_1.py
@@ -1,27 +1,33 @@
 from __future__ import print_function, division
 
+# Set variables for testing
+num_workers = 30
+batch_size = 64
+n_epochs = 100
+lr = 4e-4
+save_path = './model/resnet18_100'
+
+print('num_workers:', num_workers)
+print('batch_size:', batch_size)
+print('n_epochs:', n_epochs)
+
+import multiprocessing
+print('CPUs:', multiprocessing.cpu_count(), '\n')
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 import numpy as np
 from torchvision import transforms, utils, datasets
 import torchvision.models as models
 from tqdm import tqdm
-from torch.nn.parameter import Parameter
 import pdb
-import torchvision
-import os
 import sys
-import gzip
-import tarfile
 import gc
-from PIL import Image
-import pandas as pd
 from skimage import io, transform
-import matplotlib.pyplot as plt
-import re
+from torch.nn.parameter import Parameter
 
 from IPython.core.ultratb import AutoFormattedTB
 
@@ -29,20 +35,23 @@ from RecursionDS import RecursionDataset
 
 # from torchvision import transforms, utils, datasets
 
-assert torch.cuda.is_available(), "You need to request a GPU from Runtime > Change Runtime"
+assert torch.cuda.is_available() # GPU must be available
 
 test_dataset = RecursionDataset(csv_file1='../recursion_data/train-labels/train.csv', root_dir='../recursion_data/train-data', csv_file2='../recursion_data/train-labels/train_controls.csv')
 
-model = models.resnet18(pretrained=True)
+model = models.resnet18(pretrained=False)
+model.load_state_dict(torch.load('resnet18-5c106cde.pth'))
+
 model.conv1 = nn.Conv2d(6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-model.fc = nn.Linear(in_features=512, out_features=1508, bias=True)
-'''model = models.vgg16(pretrained=True)
-model.features[0] = nn.Conv2d(6, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
-model.classifier[6] = nn.Linear(in_features=4096, out_features=test_dataset.num_classes, bias=True)'''
+model.fc = nn.Linear(in_features=512, out_features=test_dataset.n_classes, bias=True)
+
+if torch.cuda.device_count() > 1:
+  print('Using', torch.cuda.device_count(), 'GPUs')
+  model = nn.DataParallel(model)
 
 model.cuda()
 objective = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
+optimizer = optim.Adam(model.parameters(), lr=lr)
 
 losses = []
 full_loss = []
@@ -50,8 +59,8 @@ full_acc = []
 
 loss_report = 0
 acc_report = 0
-for epoch in range(5):
-  dataloader = DataLoader(test_dataset, batch_size=16, pin_memory=True, shuffle=True)
+for epoch in range(n_epochs):
+  dataloader = DataLoader(test_dataset, batch_size=batch_size, pin_memory=True, shuffle=True, num_workers=num_workers)
   loop = tqdm(total=len(dataloader), position=0, file=sys.stdout)
 
   for batch, (x, y_truth) in enumerate(dataloader):
@@ -66,7 +75,7 @@ for epoch in range(5):
 
     loss.backward()
 
-    losses.append(loss)
+    #losses.append(loss)
 
     loop.set_description('epoch:{}'.format(epoch))
     loop.update(1)
@@ -80,14 +89,14 @@ for epoch in range(5):
   print('Predicted: ', ' '.join('%5s' % predicted[j].item() for j in range(4)))
 
 # Model Saving
-#state = {
-#    "epoch": 10,
-#    "state_dict": model.state_dict(),
-#    "optimizer": optimizer.state_dict()
-#}
-#torch.save(state, "./model")
+state = {
+    "epoch": n_epochs,
+    "state_dict": model.state_dict(),
+    "optimizer": optimizer.state_dict()
+}
+torch.save(state, save_path)
 
-#state_loaded = torch.load("./model")
+#state_loaded = torch.load("../jsCode/model/resnet18")
 ## To Restore
 #model_loaded = models.resnet18(pretrained=True)
 #model_loaded.conv1 = nn.Conv2d(6, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
@@ -101,7 +110,8 @@ loop = tqdm(total=len(dataloader), position=0)
 for images, labels in dataloader:
   images, labels = images.cuda(), labels.cuda()
   labels = labels.type(torch.cuda.LongTensor).squeeze(1)
-  y_hat = model_loaded(x)
+  #y_hat = model_loaded(x)
+  y_hat = model(x)
   _, predicted = torch.max(y_hat, 1)
   total += labels.size(0)
   correct += (predicted == y_truth).sum().item()
