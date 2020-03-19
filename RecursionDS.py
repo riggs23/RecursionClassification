bb201319f178e6bb0586e3bf3485c061baa54dbf
Skyler <skylerg022@gmail.com>
2020-03-19 15:09:12 -0600

    Update RecursionDS.py

--- RecursionDS.py
+++ RecursionDS.py
@@ -5,7 +5,7 @@ import pandas as pd
 from skimage import io
 import numpy as np
 from torch.utils.data import Dataset
-from torchvision import transforms as trfm
+from torchvision import transforms
 # from torchvision import transforms, utils, datasets
 import re
 import random
